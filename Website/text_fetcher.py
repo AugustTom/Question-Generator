b075ce67481af4274fa42717839441a856232a7d
Auguste <auguste.tomaseviciute@gmail.com>
2020-11-30 14:11:25 +0000

    Updated class imports

--- Website/text_fetcher.py
+++ Website/text_fetcher.py
@@ -1,18 +1,18 @@
 import wikipedia as wk
-import pathlib
+# import pathlib
 
 # TODO add topic checker to make sure it picks one
 class TextFetcher:
     def __init__(self, title):
-        p = pathlib.Path('./Texts/{}.txt'.format(title))
-        if p.is_file():
-            print("File is already present. Reading from the file")
-            self.text = open(p).read()
-        else:
-            self.text = wk.page(title).summary
-            file = open(p, "w")
-            file.write(self.text)
-            file.close()
+        # p = pathlib.Path('./Texts/{}.txt'.format(title))
+        # if p.is_file():
+        #     print("File is already present. Reading from the file")
+        #     self.text = open(p).read()
+        # else:
+        self.text = wk.page(title).summary
+        file = open(p, "w")
+        file.write(self.text)
+        file.close()
 
 
     def getText(self):
