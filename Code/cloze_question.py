3f67961c5e3b4ef1fc52d25f8c042cb508c7d1bb
Auguste <auguste.tomaseviciute@gmail.com>
2020-11-30 14:12:47 +0000

    Website changes

--- Code/cloze_question.py
+++ Code/cloze_question.py
@@ -4,4 +4,12 @@ class ClozeQuestion:
         self.question = question
         self.answer = answer
         self.distractors = distractors
+        self.choices =[]
+        self.choices.append(answer)
+        self.choices.append(distractors)
+
+    def get_quiz(self):
+        pass
+
+
 
